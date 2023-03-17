ba9b7b8ec754fb35e75fa096aa08f3d44de8febb
davirmrm <davi.ribeiro@fdte.io>
2023-03-17 19:56:36 -0300

    ajust

--- form/calendar/calendar.js
+++
@@ -1,2 +0,0 @@
-export * from './calendarYear'
-export * from './selectCalendar'
--- form/form.js
+++
@@ -1,9 +0,0 @@
-import './form.scss';
-
-// export * from './calendar/calendar'
-export * from './checkbox/checkbox'
-export * from './input/input'
-export * from './radiobuton/radiobutton'
-export * from './select/select'
-
-export * from './Mask';
