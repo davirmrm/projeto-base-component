b270f225ccde50b085370b0854027b27f737829f
davirmrm <davi.ribeiro@fdte.io>
2023-03-17 18:22:21 -0300

    ajust test

--- src/config/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import alerts from '../modules/alert/alertsRedux'
-
-export const rootReducer = combineReducers({
-  alerts,
-})
-
-export default rootReducer
